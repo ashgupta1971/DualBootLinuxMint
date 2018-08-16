f9c64eb831a9367b59102fb75f5b87479ea04e81
Ash Gupta <ashgupta3@gmail.com>
2018-08-15 22:33:46 -0400

    Updated visitor account creation under Windows.

--- Common/create-visitor-account.bat
+++ Common/create-visitor-account.bat
@@ -4,11 +4,11 @@ REM Source: Maximum PC November 2016
 REM This batch file creates a "guest" user called "visitor".
 REM It must be run in an administrator window.
 
-net user visitor /add /active:yes
-net user visitor *
+net user visitor * /add
+net user visitor /active:yes
 
 REM Press <ENTER> twice when prompted for a password
 REM in order to create a blank password.
 
-net localgroup users visitor /delete
-net localgroup guests visitor /add
+net localgroup Users visitor /delete
+net localgroup Guests visitor /add
